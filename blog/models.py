5bcc7e55601f4297126b04402562839bf85bd381
Emily Somach <emiksoma@gmail.com>
2020-05-01 18:39:02 -0400

    add class, properties, methods to blog models file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+#lines that add some bits from other files
 
-# Create your models here.
+
+class Post(models.Model):
+    # defines our model (an object) named Post
+    # models.Model means that Post is a Django Model, 
+    # so Django knows to save it in the database
+    
+    # properties:
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # ForeignKey is a link to another model
+    title = models.CharField(max_length=200) 
+    # CharField(max_length=XXX) = how to define text with a limited number of characters
+    text = models.TextField()
+    # TextField() = long text with no limit
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    # methods:
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
