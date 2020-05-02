112a93f7c5fd93674e1e5e765814b67bc2e28cba
Emily Somach <emilysomach@gmail.com>
2020-05-02 05:40:26 -0400

    added blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
+
+'''
+- post/ means that the URL should begin with the word post followed by a /.
+- <int:pk> means that Django expects an integer value and will transfer it to a view as a variable called pk.
+- / then we need a / again before finishing the URL.
+'''
